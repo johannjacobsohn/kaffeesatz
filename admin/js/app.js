154ead885a1dda6b030d6d23885f3e34cdc7962d
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-02-11 10:23:06 +0100

    Admin: initial version

---
+++ admin/js/app.js
@@ -0,0 +1,118 @@
+/**
+ * 
+ * 
+ */
+(function($){
+	var User = Backbone.Model.extend({
+		defaults: {
+			name: 'Klaus',
+			beverages: { Tee: 1, "Kaffee": 4 }
+		},
+		idAttribute: "_id", // MongoDB-Style
+		url: function(){
+			return 'http://localhost:1234/users/'+this.get("name")
+		}
+	});
+
+	var Users = Backbone.Collection.extend({
+		model: User,
+		url: 'http://localhost:1234/users',
+		comparator: function(user){
+			return user.get('name');
+		}
+	});
+
+	users = new Users();
+	var socket = io.connect('http://localhost');
+	socket.on('userChanged', function (data) {
+		users.where({name: data.name})[0].set(data);
+	});
+
+	var ItemView = Backbone.View.extend({
+		tagName: 'tr', // name of tag to be created        
+		events: { 
+			'click span.delete': 'remove',
+			'click span.clear': 'clear'
+		},
+		initialize: function(){
+			_.bindAll(this, 'render', 'unrender', 'clear', 'remove'); // every function that uses 'this' as the current object should be in here
+
+			this.model.bind('change', this.render);
+			this.model.bind('remove', this.unrender);
+		},
+		render: function(){
+			var beverages = this.model.get("beverages") || {};
+			var beveragesStr = Object.keys( beverages ).map(function(key){
+				return key + ": " + beverages[key]
+			}).join(", ");
+			var cost = 0;
+			var cost = Object.keys( beverages )
+				.map(function(key){
+					return beverageCollection.where({name: key})[0].get("price")*beverages[key]; })
+				.reduce( function(a, b){ return a+b; }, 0);
+			var userTemplate = _.template( $("#user_template").html(), {name: this.model.get('name'), beverages: beveragesStr, cost: cost} );
+			$(this.el).html( userTemplate );
+			return this; // for chainable calls, like .render().el
+		},
+		unrender: function(){
+			$(this.el).remove();
+		},
+		clear: function(){
+			this.model.save({"beverages": {}}, {
+				success: function() { console.log("success"); },
+				error  : function() { console.log("error");   }
+			});
+		},
+		remove: function(){
+			this.model.destroy();
+		}
+	});
+
+	var UsersView = Backbone.View.extend({
+		el: $('#users-tab'), // el attaches to existing element
+		events: {
+			'click button#add-user': 'addUser'
+		},
+		initialize: function(){
+			_.bindAll(this, 'render', 'addUser', 'appendItem'); // every function that uses 'this' as the current object should be in here
+
+			this.collection = users;
+			this.collection.bind('add', this.render); // collection event binder
+
+			this.collection.fetch({ success: this.render });
+		},
+		render: function(){
+			var that = this;
+			$("#users").empty();
+			_(this.collection.models).each(function(item){
+				that.appendItem(item);
+			}, this);
+		},
+		addUser: function(){
+			// @TODO: check for unique name
+			var that = this;
+			var modal = $('#user-modal').modal({}).on("submit", function(e){
+				e.preventDefault();
+				var name = $(this).find("input").val();
+				that.collection.create( new User({name:name}), {
+					success: function(){
+						$(modal).modal("hide");
+					},
+					error: function(){
+						console.log("error")
+					}
+				});
+			}).one("shown", function(){
+				$(this).find("input").val("").focus();
+			});
+		},
+		appendItem: function(item){
+			var itemView = new ItemView({
+				model: item
+			});
+			$('#users', this.el).append(itemView.render().el);
+		}
+	});
+
+	var usersView = new UsersView();
+})(jQuery);
---
+++ admin/js/beverages.js
@@ -0,0 +1,99 @@
+/**
+ * Beverages
+ * 
+ */
+	var Beverage = Backbone.Model.extend({
+		defaults: {
+			name: '',
+			price: 0
+		},
+		idAttribute: "_id",
+		url: function(){
+			return 'http://localhost:1234/beverages/'+this.get("name")
+		}
+	});
+
+	var Beverages = Backbone.Collection.extend({
+		model: Beverage,
+		url: 'http://localhost:1234/beverages',
+		comparator: function(user){
+			return user.get('name');
+		}
+	});
+	
+	var beverageCollection = new Beverages();
+
+(function($){
+	var BeverageView = Backbone.View.extend({
+		tagName: 'tr', // name of tag to be created        
+		events: { 
+			'click span.delete'    : 'remove',
+			'click span.set-price' : 'setPrice'
+		},
+		initialize: function(){
+			_.bindAll(this, 'render', 'unrender', 'setPrice', 'remove'); // every function that uses 'this' as the current object should be in here
+
+			this.model.bind('change', this.render);
+			this.model.bind('remove', this.unrender);
+		},
+		render: function(){
+			var userTemplate = _.template( $("#beverage_template").html(), {name: this.model.get('name'), price: this.model.get("price")} );
+			$(this.el).html( userTemplate );
+			return this; // for chainable calls, like .render().el
+		},
+		unrender: function(){
+			$(this.el).remove();
+		},
+		setPrice: function(price){
+			this.model.save({price: price})
+		},
+		remove: function(){
+			this.model.destroy();
+		}
+	});
+
+	var BeveragesView = Backbone.View.extend({
+		el: $('#beverages-tab'), // el attaches to existing element
+		events: {
+			'click button#add-beverage': 'addBeverage'
+		},
+		initialize: function(){
+			_.bindAll(this, 'render', 'addBeverage', 'appendBeverage'); // every function that uses 'this' as the current object should be in here
+
+			this.collection = beverageCollection;
+			this.collection.bind('add', this.render); // collection event binder
+
+			this.collection.fetch({ success: this.render });
+		},
+		render: function(){
+			var that = this;
+			$("#beverages").empty();
+			_(this.collection.models).each(function(item){
+				that.appendBeverage(item);
+			}, this);
+		},
+		addBeverage: function(){
+			// @TODO: check for unique name
+			var that = this;
+			var modal = $('#beverage-modal').modal({}).on("submit", function(e){
+				e.preventDefault();
+				var name = $(this).find("input.name").val();
+				var price = $(this).find("input.price").val();
+				that.collection.create( new Beverage({name:name, price:price}), {
+					success: function(){ $(modal).modal("hide"); },
+					error  : function(){ console.log("error")    }
+				});
+			}).one("shown", function(){
+				$(this).find("input").val("").first().focus();
+			});
+		},
+		appendBeverage: function(item){
+			var beverageView = new BeverageView({
+				model: item
+			});
+			$('#beverages', this.el).append(beverageView.render().el);
+		}
+	});
+
+	var beveragesView = new BeveragesView();
+})(jQuery);
