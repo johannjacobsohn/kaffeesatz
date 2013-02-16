31a295cd5c761a157128f7cc8862aab04c0cbc83
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-02-16 14:38:16 +0100

    server: clean up db.js

--- server/db.js
+++ server/db.js
@@ -1,38 +1,33 @@
-// appfog
+/*
+ * MongoDB configuration
+ * 
+ */
+ 
+// Default configuration
+var mongo = {
+	"hostname" : "127.0.0.1",
+	"port"     : 27017,
+	"username" : "",
+	"password" : "",
+	"name"     : "coffeedb",
+	"db"       : "coffeedb"
+};
+
+// rewrite credentials for appfog
 if(process.env.VCAP_SERVICES){
 	var env = JSON.parse(process.env.VCAP_SERVICES);
-	var mongo = env['mongodb-1.8'][0].credentials;
-} else if(process.env.TRAVIS){
-	var mongo = {
-		"hostname":"127.0.0.1",
-		"port":27017,
-		"username":"",
-		"password":"",
-		"name":"coffeedb",
-		"db":"db"
-	};
-} else {
-	var mongo = {
-		"hostname":"localhost",
-		"port":27017,
-		"username":"",
-		"password":"",
-		"name":"coffeedb",
-		"db":"db"
-	};
+	mongo = env['mongodb-1.8'][0].credentials;
+} else if(process.env.KAFFEESATZTEST){
+	// local testing - set db to coffeedb_test to be dropped later
+	mongo.db = "coffeedb_test";
 }
-var generate_mongo_url = function(obj){
-	obj.hostname = (obj.hostname || 'localhost');
-	obj.port = (obj.port || 27017);
-	obj.db = (obj.db || 'test');
-	if(obj.username && obj.password){
-		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-	}
-	else{
-		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-	}
-};
-var mongourl = generate_mongo_url(mongo);
+
+var mongourl;
+if( mongo.username && mongo.password ){
+	mongourl = "mongodb://" + mongo.username + ":" + mongo.password + "@" + mongo.hostname + ":" + mongo.port + "/" + mongo.db;
+} else {
+	mongourl = "mongodb://" + mongo.hostname + ":"                                               + mongo.port + "/" + mongo.db;
+}
 
 var collections = ["beverages", "users"];
 var db = require("mongojs").connect(mongourl, collections);
