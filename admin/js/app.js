4950af78dc643bb675e4b1292450dfb272e7019f
Johann Jacobsohn <johann.jacobsohn@directbox.com>
2013-02-17 22:46:14 +0100

    Admin: clean up html, improve beverages, introduce app.js for everthing that doesn't fit into users.js or beverages.js

---
+++ admin/js/app.js
@@ -0,0 +1,88 @@
+function formatNumber(n, precision){
+precision = precision || 2;
+var p = Math.pow(10, precision);
+n = n || 0; // Test "Kein Argument sollte 0,00 zurückgeben; "
+n = parseFloat(n.toString().replace(",",".")); // Test "String im korrekten Format"
+n = Math.round(n * p)/p;
+return n.toFixed( precision ).toString().replace(".",",");
+}
+
+// thanks Bootstrap...!
+!(function(){
+	var hash = document.location.hash, prefix = "tab_";
+	if (hash) {
+		$('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+	} 
+
+	// Change hash for page-reload
+	$('.nav-tabs a').on('shown', function (e) {
+		window.location.hash = e.target.hash.replace("#", "#" + prefix);
+	});
+})();
+
+(function(){
+	var showing = false;
+	$(".show-help").click(function(e){
+		e.preventDefault();
+		$(".help").popover( showing ? "hide" : "show" );
+		showing = !showing;
+	});
+})();
+
+(function(){
+	$(".install-app").toggle(!!navigator.mozApps).click(function(){
+		var index = window.location.href.lastIndexOf("/");
+		if(index > window.location.href.indexOf("://")+2) {
+			url = window.location.href.substring(0, index+1) + "manifest.webapp";
+		} else {
+			url = window.location.href + "/manifest.webapp";
+		}
+		var installRequest = navigator.mozApps.install(url);
+		installRequest.onsuccess = function(){
+			alert("wurde installiert!")
+		};
+		installRequest.onerror = function(){
+			alert("konnte nicht installiert werden!")
+		};
+	});
+	if(navigator.mozApps) {
+		var request = navigator.mozApps.getSelf();
+		request.onsuccess = function (response) {
+			if(request.result){
+				$(".install-app").hide();
+			}
+		};
+	}
+})();
+
+/*
+ * Zahlen in einen String formieren,
+ * der zwei (oder precision) Nachkommastellen hat 
+ * und Komma als Dezimaltrennzeichen verwendet (nach SI)
+ *
+ * @TODO: Tests für andere Präzision
+ * @example:
+	formatNumberTest = function(){
+		console.assert(formatNumber(07) === "7,00", "Führende Null");
+		console.assert(formatNumber(121212128.1212121212) === "121212128,12", "Lange Zahl");
+		console.assert(formatNumber("8,23") === "8,23", "String im korrekten Format");
+		console.assert(formatNumber("8.235") === "8,24", "korrektes aufrunden");
+		console.assert(formatNumber("8.234") === "8,23", "korrektes abrunden");
+		console.assert(formatNumber("8") === "8,00", "Ganze Zahl als String");
+		console.assert(formatNumber(0) === "0,00", "Null als Int");
+		console.assert((formatNumber()).toString() === "0,00", "Kein Argument sollte 0,00 zurückgeben");
+		console.assert((formatNumber("test").toString() === "NaN"), "Nicht parsebare Zahl sollte NaN zurückgeben");
+	}
+	formatNumberTest()
+ * @param             n die zu formatierende Zahl - kann String, Int oder Float sein
+ * @param  {{int}}    precision (optional) - Anzahl an Nachkommastellen, voreingestellt sind 2 
+ * @return {{string}} der formatierte String
+ */
+var formatNumber = function(n, precision){
+	precision = precision || 2;
+	var p = Math.pow(10, precision);
+	n = n || 0; // Test "Kein Argument sollte 0,00 zurückgeben; "
+	n = parseFloat(n.toString().replace(",",".")); // Test "String im korrekten Format"
+	n = Math.round(n * p)/p;
+	return n.toFixed( precision ).toString().replace(".",",");
+};
--- admin/js/beverages.js
+++ admin/js/beverages.js
@@ -7,7 +7,7 @@
 			name: '',
 			price: 0
 		},
-		idAttribute: "_id",
+		idAttribute: "_id", // MongoDB-Style
 		url: function(){
 			return '/beverages/'+this.get("name");
 		}
@@ -16,20 +16,32 @@
 	var Beverages = Backbone.Collection.extend({
 		model: Beverage,
 		url: '/beverages',
-		comparator: function(user){
-			return user.get('name');
+		comparator: function(beverage){
+			return beverage.get('name');
 		}
 	});
 	
-	var beverageCollection = new Beverages();
+	Beverages.prototype.create = function(beverage, events) {
+		var isDupe = this.any(function(_beverage) { 
+			return _beverage.get('name') === beverage.get('name');
+		});
+		if ( isDupe || !beverage.get('name') || isNaN(beverage.get('price')) ) {
+			events.error();
+			return false;
+		}
+		Backbone.Collection.prototype.create.apply(this, arguments);
+	}
 
+	var beverageCollection = new Beverages();
+	
 (function($){
 	"use strict";
+
 	var BeverageView = Backbone.View.extend({
 		tagName: 'tr',
 		events: {
-			'click span.delete'    : 'remove',
-			'click span.set-price' : 'setPrice'
+			'click .delete'    : 'remove',
+			'click .set-price' : 'setPrice'
 		},
 		initialize: function(){
 			_.bindAll(this, 'render', 'unrender', 'setPrice', 'remove'); // every function that uses 'this' as the current object should be in here
@@ -38,7 +50,7 @@
 			this.model.bind('remove', this.unrender);
 		},
 		render: function(){
-			var userTemplate = _.template( $("#beverage_template").html(), {name: this.model.get('name'), price: this.model.get("price")} );
+			var userTemplate = _.template( $("#beverage_template").html(), {name: this.model.get('name'), price: formatNumber(this.model.get("price"))} );
 			$(this.el).html( userTemplate );
 			return this; // for chainable calls, like .render().el
 		},
@@ -56,7 +68,11 @@
 	var BeveragesView = Backbone.View.extend({
 		el: $('#beverages-tab'), // el attaches to existing element
 		events: {
-			'click button#add-beverage': 'addBeverage'
+			'click button#add-beverage': 'showAddBeverage',
+			'submit #beverage-modal': "addBeverage",
+			'show #beverage-modal': "modalShown",
+			'keyup #beverage-modal input.name': "checkBeverage",
+			'keyup #beverage-modal input.price': "checkPrice"
 		},
 		initialize: function(){
 			_.bindAll(this, 'render', 'addBeverage', 'appendBeverage'); // every function that uses 'this' as the current object should be in here
@@ -67,26 +83,76 @@
 			this.collection.fetch({ success: this.render });
 		},
 		render: function(){
-			var that = this;
 			$("#beverages").empty();
 			_(this.collection.models).each(function(item){
-				that.appendBeverage(item);
+				this.appendBeverage(item);
 			}, this);
 		},
-		addBeverage: function(){
-			// @TODO: check for unique name
-			var that = this;
-			var modal = $('#beverage-modal').modal({}).on("submit", function(e){
-				e.preventDefault();
-				var name = $(this).find("input.name").val();
-				var price = $(this).find("input.price").val();
-				that.collection.create( new Beverage({name:name, price:price}), {
-					success: function(){ $(modal).modal("hide"); },
-					error  : function(){ console.log("error");   }
-				});
-			}).one("shown", function(){
-				$(this).find("input").val("").first().focus();
+		showAddBeverage: function(){
+			this.modal = $('#beverage-modal').modal({})
+		},
+		addBeverage: function(e){
+			e.preventDefault();
+			var name = $("#beverage-modal input.name").val();
+			var price = this.parseNumber( $("#beverage-modal input.price").val() );
+			this.collection.create( new Beverage({name:name, price:price}), {
+				success: function(){
+					$(this.modal).modal("hide");
+				}.bind(this),
+				error: function(){
+					console.log("error");
+				}
 			});
+		},
+		modalShown: function(){
+			var $beverageModal = $("#beverage-modal");
+			$beverageModal.find(".price-nan").hide();
+			$beverageModal.find("input.price, input.name").val("").focus();
+		},
+		checkBeverage: function(e){
+			var name = $("#beverage-modal input.name").val();
+			$("#beverage-modal .beverage-exists").toggle( this.collection.where({name:name}).length > 0);
+		},
+		checkPrice: function(e){
+			var price = this.parseNumber( $("#beverage-modal input.price").val() || 0 );
+			var isParsable = !isNaN( price );
+			$("#beverage-modal .price-nan").toggle( !isParsable );
+			$("#beverage-modal .parsedPrice").toggle( isParsable ).find(".price").text( this.formatNumber(price) );
+		},
+		parseNumber: function(n){
+			n = n.toString().replace(",", ".");
+			return parseFloat(n);
+		},
+		/*
+		 * Zahlen in einen String formieren,
+		 * der zwei (oder precision) Nachkommastellen hat 
+		 * und Komma als Dezimaltrennzeichen verwendet (nach SI)
+		 *
+		 * @TODO: Tests für andere Präzision
+		 * @example:
+			formatNumberTest = function(){
+				console.assert(formatNumber(07) === "7,00", "Führende Null");
+				console.assert(formatNumber(121212128.1212121212) === "121212128,12", "Lange Zahl");
+				console.assert(formatNumber("8,23") === "8,23", "String im korrekten Format");
+				console.assert(formatNumber("8.235") === "8,24", "korrektes aufrunden");
+				console.assert(formatNumber("8.234") === "8,23", "korrektes abrunden");
+				console.assert(formatNumber("8") === "8,00", "Ganze Zahl als String");
+				console.assert(formatNumber(0) === "0,00", "Null als Int");
+				console.assert((formatNumber()).toString() === "0,00", "Kein Argument sollte 0,00 zurückgeben");
+				console.assert((formatNumber("test").toString() === "NaN"), "Nicht parsebare Zahl sollte NaN zurückgeben");
+			}
+			formatNumberTest()
+		 * @param             n die zu formatierende Zahl - kann String, Int oder Float sein
+		 * @param  {{int}}    precision (optional) - Anzahl an Nachkommastellen, voreingestellt sind 2 
+		 * @return {{string}} der formatierte String
+		 */
+		formatNumber: function(n, precision){
+			precision = precision || 2;
+			var p = Math.pow(10, precision);
+			n = n || 0; // Test "Kein Argument sollte 0,00 zurückgeben; "
+			n = parseFloat(n.toString().replace(",",".")); // Test "String im korrekten Format"
+			n = Math.round(n * p)/p;
+			return n.toFixed( precision ).toString().replace(".",",");
 		},
 		appendBeverage: function(item){
 			var beverageView = new BeverageView({
@@ -97,4 +163,6 @@
 	});
 
 	var beveragesView = new BeveragesView();
-})(jQuery);
+}(jQuery));
+
+
