70c4f44ac31afe18c14869de19810e1a178a1632
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-03-24 23:37:09 +0100

    Fix tally list class when displaying 0 beverages

--- app/source/controllers/UserController.js
+++ app/source/controllers/UserController.js
@@ -2,13 +2,14 @@ enyo.kind({
 	name: "Coffee.UserController",
 	kind: "enyo.ModelController",
 	corresondingCount: enyo.Computed(function () {
-		enyo.log("recalculate count");
-		var count = "Ompalompa";
+		var count = 0;
 		if(this.data) {
 			enyo.log("calculated", this.data.get("name"), this.data.get("correspondingBeverage"));
 			var correspondingBeverage = this.data.get("correspondingBeverage");
 			var beverages = this.data.get("beverages");
 			count = beverages[correspondingBeverage];
+		} else {
+			enyo.warn("correspondingCount: this.data not available");
 		}
 		return count;
 	}, "beverages"),
